4b16d7af7f5071ec405596c760a24aeef735e6f9
Woomy4680-exe <matzar1808@gmail.com>
2020-10-31 19:22:51 +0100

    Moved to fucntions

--- install.sh
+++ install.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
-if [[ ! "$(command -v zsh)" ]]; then
-	echo -e "\e[91mZSH is not installed... Please install it with your Package manager!"
-	exit 1	
-fi
+function Check_ZSH {
+	if [[ ! "$(command  -v zsh)" ]]; then
+		echo -e "\e[91mZSH is not installed... Please install it with your package manager!"
+		exit 1
+	fi
+}
+function Check_Inet {
 ping -c 1 kernel.org > /dev/null
 if [[ $? != 0 ]]; then
-	echo -e "\e[91mYou are not connected to internet! Please check your internet connection and retry!";
-	exit 1;
+    echo -e "\e[91mYou are not connected to internet! Please check your internet connection and retry!";
+    exit 1;
 fi
+}
+
+function main {
+    Check_ZSH;
+    Check_Inet;
+}
+
