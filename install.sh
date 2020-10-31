47fb2029fe146b396def7922e9f27f7113ed6a35
Woomy4680-exe <matzar1808@gmail.com>
2020-10-31 19:29:56 +0100

    Renamed functions

--- install.sh
+++ install.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash
-function Check_ZSH {
+function Check-ZSH {
 	if [[ ! "$(command  -v zsh)" ]]; then
 		echo -e "\e[91mZSH is not installed... Please install it with your package manager!"
 		exit 1
 	fi
 }
-function Check_Inet {
+function Check-Inet {
 ping -c 1 kernel.org > /dev/null
 if [[ $? != 0 ]]; then
     echo -e "\e[91mYou are not connected to internet! Please check your internet connection and retry!";
     exit 1;
 fi
 }
-
 function main {
-    Check_ZSH;
-    Check_Inet;
+    Check-ZSH;
+    Check-Inet;
 }
 
