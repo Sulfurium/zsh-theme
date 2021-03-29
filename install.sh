39bfabdf1da89d64e5086c00ccbde1efe0914407
Woomy4680-exe <matzar1808@gmail.com>
2021-03-29 11:00:45 +0200

    Installer: Make install compatible with posix shell

--- install.sh
+++ install.sh
@@ -1,34 +1,40 @@
 #!/usr/bin/env bash
-function Check-ZSH {
+
+checkzsh() {
 	if [[ ! "$(command  -v zsh)" ]]; then
 		echo -e "\e[91mZSH is not installed... Please install it with your package manager!"
 		exit 1
 	fi
 }
-function Check-Wget {
+
+checkwget() {
     if [[ ! "$(command  -v wget)" ]]; then
 	echo -e "\e[91mWget is not installed... Please install it with your package manager!"
 	exit 1
     fi
 }
-function Check-Tar {
+
+checktar() {
     if [[ ! "$(command -v tar)" ]]; then
 	echo -e "\e[91mTar is not installed... Please install it with your package manager!"
 	exit 1
     fi
 }
-function Check-Inet {
+
+checkinet() {
     ping -c 1 kernel.org > /dev/null || {
 	echo -e "\e[91mYou are not connected to internet! Please check your internet connection and retry!";
 	exit 1;
     }
 }
-function Create-Testdir {
+
+createtempdir() {
     printf "\e[96mCreating temp directory...\r"
     TEMPDIR=`mktemp -d`
     echo -e "\e[94mCreated temp directory ${TEMPDIR}"
 }
-function Download-Archive {
+
+downloadarchive() {
     printf "\e[96mDownloading theme...\r"
     wget https://github.com/Sulfurium/zsh-theme/archive/main.tar.gz -O "${TEMPDIR}/theme.tar.gz" -o /dev/null
     if [[ $? != 0 ]]; then
@@ -37,7 +43,8 @@ function Download-Archive {
     fi
     echo -e "\e[94mDownloaded Sucessfully!"
 }
-function Extract-Theme {
+
+extracttheme() {
     printf "\e[96mExtracting theme...\r"
     tar xf "${TEMPDIR}/theme.tar.gz" -C "${TEMPDIR}" 2> /dev/null
     if [[ $? != 0 ]]; then
@@ -46,7 +53,8 @@ function Extract-Theme {
     fi
     echo -e "\e[94mTheme extracted succesfully!"
 }
-function Install-Theme {
+
+installtheme() {
     printf "\e[96mInstalling theme...\r"
     if [[ -d "${HOME}/.oh-my-zsh" ]]; then
 	OMZ_PRESENT="true"
@@ -59,7 +67,8 @@ function Install-Theme {
     mv "${TEMPDIR}/zsh-theme-main/sulfurium.zsh-theme" "${INSDIR}/"
     echo -e "\e[94mTheme successfully installed."
 }
-function Setup-ZSHRC {
+
+setupzshrc() {
     echo -e "\e[96mInstalling new ZSHRC..."
     if [[ -f "${HOME}/.zshrc" ]]
     then 
@@ -74,19 +83,22 @@ function Setup-ZSHRC {
     cp "${TEMPDIR}/zsh-theme-main/zshrcs/${RC_FILE}" "${HOME}/.zshrc"
     echo -e "\e[92mTheme successfully installed!"
 }
-function Clean-TMPdir {
+
+cleantempdir() {
     rm -rf "${TEMPDIR}"
 }
-function main {
-    Check-ZSH;
-    Check-Inet;
-    Check-Wget;
-    Check-Tar;
-    Create-Testdir
-    Download-Archive;
-    Extract-Theme;
-    Install-Theme;
-    Setup-ZSHRC;
-    Clean-TMPdir;
+
+main() {
+    checkzsh
+    checkinet
+    checkwget
+    checktar
+    createtempdir
+    downloadarchive
+    extracttheme
+    installtheme
+    setupzshrc
+    cleantempdir
 }
-main;
+
+main
